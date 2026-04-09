b156ee08a241b1154311af34c53048ee7b53ef74
Jordan Lee Brand <jordanleebrand@gmail.com>
2026-04-09 15:48:20 -0500

    Added functionality: create blank LWJGL window with ESC to close

--- src/voxelengine/VoxelEngine.java
+++ src/voxelengine/VoxelEngine.java
@@ -1,9 +1,85 @@
 package voxelengine;
 
-public class VoxelEngine {
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
 
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class VoxelEngine {
+    
+    // The window handle
+    private long window;
+    
+    public void run() {
+        
+        init();
+        loop();
+        
+        // Free the window callbakcs and destroy the window
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+        
+        // Terminate GLFW and free the error callback
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+    
+    private void init() {
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+        
+        // Initialize GLFW
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+        
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        
+        // Create the window
+        window = glfwCreateWindow(800, 600, "Voxel Engine", NULL, NULL);
+        if (window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window");
+        
+        // Setup key callback for closing window by pressing escape
+        glfwSetKeyCallback(window, (win, key, scancode, action, mods) -> {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+            glfwSetWindowShouldClose(win, true);
+        });
+        
+        // Make the OpenGL context current
+	glfwMakeContextCurrent(window);
+        
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+    
+    private void loop() {
+        GL.createCapabilities();
+        
+        // Set the clear color
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        
+        // Run the rendering loop until the user has attempted to close
+        // the window
+        while (!glfwWindowShouldClose(window)) {
+            // Clear the framebuffer
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            
+            // Swap the color buffers
+            glfwSwapBuffers(window);
+            
+            // Poll for window events
+            glfwPollEvents();
+        }
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        new VoxelEngine().run();
     }
     
 }
